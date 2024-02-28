a9d06763e542ae65dcff08ce0ecfa337982f5513
Laura Augusta <laurah.algusta@gmail.com>
2024-02-27 22:39:31 -0300

    init

---
+++ hello.py
@@ -0,0 +1,8 @@
+# pip install gunicorn
+def app(environ, start_response):
+        data = b"Hello, World!\n"
+        start_response("200 OK", [
+            ("Content-Type", "text/plain"),
+            ("Content-Length", str(len(data)))
+        ])
+        return iter([data])
